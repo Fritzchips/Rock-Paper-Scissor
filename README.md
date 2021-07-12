cb4fee518df5a0f37cc48c7c31426d26cbece72c
Fritzchips <fritzjohn_solibio@yahoo.ca>
2021-07-11 21:11:57 -0700

    added images folder, readme file and refactored javascript code

---
+++ README.md
@@ -0,0 +1,17 @@
+## Rock Paper Scissor Game
+
+Rock paper scissor game made with basic HTML, CSS and JavaScript
+
+Try it out [here](https://fritzchips.github.io/Rock-Paper-Scissor/)
+
+![]()
+
+## Instructions
+
+## Summary
+
+## Author
+
+---
+
+- **Fritz Solibio** - _Software Development Mastermind Student_
--- index.js
+++ index.js
@@ -1,79 +1,74 @@
-let pRock = document.getElementById('rBox');
-let pPaper = document.getElementById('pBox');
-let pScissor = document.getElementById('sBox');
-let result = document.getElementById('results');
-let visual = document.getElementById('visual');
-let score = document.getElementById('score');
+const pickRock = document.getElementById("rBox");
+const pickPaper = document.getElementById("pBox");
+const pickScissor = document.getElementById("sBox");
+const result = document.getElementById("results");
+const visual = document.getElementById("visual");
+const score = document.getElementById("score");
 
-let array = ['rock', 'paper', 'scissor'];
-let player;
-let computer;
-
-let outcome;
+let array = ["rock", "paper", "scissor"];
+let player, computer, outcome;
 let win = 0;
 let lose = 0;
 let draw = 0;
 
-pRock.addEventListener('click', () => {
-    player = array[0];
-    computer = array[Math.floor(Math.random() * 3)];
+pickRock.addEventListener("click", () => {
+  player = array[0];
+  computer = array[Math.floor(Math.random() * 3)];
 
-    if(computer == array[0]) {
-        outcome = 'got a Draw';
-    } else if (computer == array[1]) {
-        outcome = 'Lose!';
-    } else {
-        outcome = 'Win!';
-    }
+  if (computer == array[0]) {
+    outcome = "got a Draw";
+  } else if (computer == array[1]) {
+    outcome = "Lose!";
+  } else {
+    outcome = "Win!";
+  }
 
-    judge();
-})
+  judge();
+});
 
-pPaper.addEventListener('click', () => {
-    player = array[1];
-    computer = array[Math.floor(Math.random() * 3)];
+pickPaper.addEventListener("click", () => {
+  player = array[1];
+  computer = array[Math.floor(Math.random() * 3)];
 
-    if(computer == array[0]) {
-        outcome = 'Win!';
-    } else if (computer == array[1]) {
-        outcome = 'got a Draw';
-    } else {
-        outcome = 'Lose!';
-    }
+  if (computer == array[0]) {
+    outcome = "Win!";
+  } else if (computer == array[1]) {
+    outcome = "got a Draw";
+  } else {
+    outcome = "Lose!";
+  }
 
-    judge();
-})
+  judge();
+});
 
-pScissor.addEventListener('click', () => {
-    player = array[2];
-    computer = array[Math.floor(Math.random() * 3)];
+pickScissor.addEventListener("click", () => {
+  player = array[2];
+  computer = array[Math.floor(Math.random() * 3)];
 
-    if(computer == array[0]) {
-        outcome = 'Lose!';
-    } else if (computer == array[1]) {
-        outcome = 'Win!';
-    } else {
-        outcome = 'got a Draw'; 
-    }
+  if (computer == array[0]) {
+    outcome = "Lose!";
+  } else if (computer == array[1]) {
+    outcome = "Win!";
+  } else {
+    outcome = "got a Draw";
+  }
 
-    judge();
-})
+  judge();
+});
 
 function judge() {
-    
-    if(outcome == 'Win!') {
-        visual.style.color = 'green';
-        win ++;
-    } else if(outcome == 'Lose!' ) {
-        visual.style.color = 'red';
-        lose ++;
-    } else if(outcome == 'got a Draw') {
-        visual.style.color = 'blue';
-        draw ++;
-    }
-    
-    result.innerText = 'You picked ' + player + '\nComputer picked ' + computer;
-    visual.innerText = 'You ' + outcome;
-    score.innerText = 'Wins: ' + win + '\nLoses: ' + lose + '\nDraws: ' + draw;  
+  if (outcome == "Win!") {
+    visual.style.color = "green";
+    win++;
+  } else if (outcome == "Lose!") {
+    visual.style.color = "red";
+    lose++;
+  } else if (outcome == "got a Draw") {
+    visual.style.color = "blue";
+    draw++;
+  }
+
+  result.innerText = `You picked ${player}\nComputer picked ${computer}`;
+  visual.innerText = `You ${outcome}`;
+  score.innerText = `Wins: ${win}\nLoses: ${lose}\nDraws: ${draw}`;
 }
-//displays all text and totals overall score
